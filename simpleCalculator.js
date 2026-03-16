12d6f492c3f1dc03fd5bcb95dd53c040ad953168
Hajara Yahaya <hajara190990@gmail.com>
2026-03-16 15:06:58 +0100

    a working simple calculator

---
+++ simpleCalculator.js
@@ -0,0 +1,31 @@
+//SIMPLE FUNCTION CALCULATOR
+//Declare variables using const
+//Create functions
+//Use arithmetic operators
+    //add two numbers
+    //multiply two numbers
+    //divide two numbers
+    //subtract two numbers
+
+const number1 = 150
+const number2 = 75
+
+function add() {
+    return number1 + number2;
+}
+console.log("addition:", add());
+
+  function multiply() {
+    return number1 * number2;
+  }
+  console.log("multiplication:", multiply());
+
+  function divide() {
+    return number2 / number1
+  }
+ console.log("division:", divide());
+
+ function subtract() {
+    return number1 - number2 
+ }
+console.log("subtraction:", subtract());
